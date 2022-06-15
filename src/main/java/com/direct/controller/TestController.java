3dd1250c213014ae45885501f4d18931f948dd72
hahaha <1020623851@qq.com>
2022-06-15 15:05:06 +0800

    one

---
+++ src/main/java/com/direct/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.direct.controller;
+
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("1===============");
+        System.out.println("2===============");
+        System.out.println("3===============");
+    }
+}
